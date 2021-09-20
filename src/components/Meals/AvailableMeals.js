c4f66382dce69adca6db5c4c2abc4a6c4d9ae054
Jubel Van Kristo Sinaga <jubelsinaga13@gmail.com>
2021-09-20 20:13:32 +0700

    Add meals data to database and fetch it

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,51 +1,49 @@
 import Card from "../UI/Card";
 import styles from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
-  const mealList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      id={meal.id}
-      key={meal.id}
-      name={meal.name}
-      descripton={meal.description}
-      price={meal.price}
-    />
-  ));
-  return (
-    <section className={styles.meals}>
-      <Card>
-        <ul>{mealList}</ul>
-      </Card>
-    </section>
-  );
+    const [meals, setmeals] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(
+                "https://react-http-8325a-default-rtdb.firebaseio.com/meals.json"
+            );
+
+            const data = await response.json();
+
+            const loadedMeals = [];
+
+            for (const key in data) {
+                loadedMeals.push({
+                    id: key,
+                    name: data[key].name,
+                    description: data[key].description,
+                    price: data[key].price,
+                });
+            }
+            setmeals(loadedMeals);
+        }
+        fetchData();
+    }, []);
+
+    const mealList = meals.map((meal) => (
+        <MealItem
+            id={meal.id}
+            key={meal.id}
+            name={meal.name}
+            descripton={meal.description}
+            price={meal.price}
+        />
+    ));
+    return (
+        <section className={styles.meals}>
+            <Card>
+                <ul>{mealList}</ul>
+            </Card>
+        </section>
+    );
 };
 
 export default AvailableMeals;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,81 +2,84 @@ import { useReducer } from "react";
 import CartContext from "./cart-context";
 
 const defaultCartState = {
-  items: [],
-  totalAmount: 0,
+    items: [],
+    totalAmount: 0,
 };
 
 const cartReducer = (state, action) => {
-  if (action.type === "ADD") {
-    const updatedAmount =
-      state.totalAmount + action.item.price * action.item.amount;
+    if (action.type === "ADD") {
+        const updatedAmount =
+            state.totalAmount + action.item.price * action.item.amount;
 
-    const existingItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id
-    );
+        const existingItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
 
-    const existingItem = state.items[existingItemIndex];
+        const existingItem = state.items[existingItemIndex];
 
-    let updatedItems;
+        let updatedItems;
 
-    if (existingItem) {
-      const updatedItem = {
-        ...existingItem,
-        amount: existingItem.amount + action.item.amount,
-      };
-      updatedItems = [...state.items];
-      updatedItems[existingItemIndex] = updatedItem;
-    } else {
-      updatedItems = state.items.concat(action.item);
+        if (existingItem) {
+            const updatedItem = {
+                ...existingItem,
+                amount: existingItem.amount + action.item.amount,
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedAmount,
+        };
     }
-    return {
-      items: updatedItems,
-      totalAmount: updatedAmount,
-    };
-  }
-  if (action.type === "REMOVE") {
-    const existingItemIndex = state.items.findIndex(
-      (item) => item.id === action.id
-    );
-    const existingItem = state.items[existingItemIndex];
-    const updatedTotalAmount = state.totalAmount - existingItem.price;
-    let updatedItems;
-    if (existingItem.amount === 1) {
-      updatedItems = state.items.filter((item) => item.id !== action.id);
-    } else {
-      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
-      updatedItems = [...state.items];
-      updatedItems[existingItemIndex] = updatedItem;
+    if (action.type === "REMOVE") {
+        const existingItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+        );
+        const existingItem = state.items[existingItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter((item) => item.id !== action.id);
+        } else {
+            const updatedItem = {
+                ...existingItem,
+                amount: existingItem.amount - 1,
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingItemIndex] = updatedItem;
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
     }
-    return {
-      items: updatedItems,
-      totalAmount: updatedTotalAmount,
-    };
-  }
-  return defaultCartState;
+    return defaultCartState;
 };
 
 const CartProvider = (props) => {
-  const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
+    const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
 
-  const addItemHandler = (item) => {
-    dispatchCart({ type: "ADD", item: item });
-  };
-  const removeItemHandler = (id) => {
-    dispatchCart({ type: "REMOVE", id: id });
-  };
+    const addItemHandler = (item) => {
+        dispatchCart({ type: "ADD", item: item });
+    };
+    const removeItemHandler = (id) => {
+        dispatchCart({ type: "REMOVE", id: id });
+    };
 
-  const cartContext = {
-    items: cartState.items,
-    totalAmount: cartState.totalAmount,
-    addItem: addItemHandler,
-    removeItem: removeItemHandler,
-  };
-  return (
-    <CartContext.Provider value={cartContext}>
-      {props.children}
-    </CartContext.Provider>
-  );
+    const cartContext = {
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
+        addItem: addItemHandler,
+        removeItem: removeItemHandler,
+    };
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    );
 };
 
 export default CartProvider;
