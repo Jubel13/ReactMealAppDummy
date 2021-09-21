7e8ef2b2b65d8a65fd799cdc12ab9ba9e9e086ff
Jubel Van Kristo Sinaga <jubelsinaga13@gmail.com>
2021-09-21 17:46:39 +0700

    Add Form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,53 +1,69 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
 import Modal from "../UI/Modal";
 import styles from "./Cart.module.css";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
-  const ctx = useContext(CartContext);
+    const [isChekcout, setIsCheckout] = useState(false);
+    const ctx = useContext(CartContext);
 
-  const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
+    const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
 
-  const hasItem = ctx.items.length > 0;
+    const hasItem = ctx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {
-    ctx.removeItem(id);
-  };
+    const cartItemRemoveHandler = (id) => {
+        ctx.removeItem(id);
+    };
 
-  const cartItemAddHandler = (item) => {
-    ctx.addItem({ ...item, amount: 1 });
-  };
+    const cartItemAddHandler = (item) => {
+        ctx.addItem({ ...item, amount: 1 });
+    };
 
-  const cartItems = (
-    <ul className={styles["cart-items"]}>
-      {ctx.items.map((item) => (
-        <CartItem
-          key={item.id}
-          name={item.name}
-          price={item.price}
-          amount={item.amount}
-          onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item)}
-        />
-      ))}
-    </ul>
-  );
-  return (
-    <Modal onClose={props.onHide}>
-      {cartItems}
-      <div className={styles.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      <div className={styles.actions}>
-        <button onClick={props.onHide} className={styles["button--alt"]}>
-          Close
-        </button>
-        {hasItem && <button className={styles.button}>Order</button>}
-      </div>
-    </Modal>
-  );
+    const cartItems = (
+        <ul className={styles["cart-items"]}>
+            {ctx.items.map((item) => (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    price={item.price}
+                    amount={item.amount}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
+            ))}
+        </ul>
+    );
+
+    const orderHandler = () => {
+        setIsCheckout(true);
+    };
+
+    const ModalActions = (
+        <div className={styles.actions}>
+            <button onClick={props.onHide} className={styles["button--alt"]}>
+                Close
+            </button>
+            {hasItem && (
+                <button className={styles.button} onClick={orderHandler}>
+                    Order
+                </button>
+            )}
+        </div>
+    );
+
+    return (
+        <Modal onClose={props.onHide}>
+            {cartItems}
+            <div className={styles.total}>
+                <span>Total Amount</span>
+                <span>{totalAmount}</span>
+            </div>
+            {isChekcout && <Checkout onCancel={props.onHide} />}
+            {!isChekcout && ModalActions}
+        </Modal>
+    );
 };
 
 export default Cart;
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,32 +4,32 @@ import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div onClick={props.onClose} className={styles.backdrop} />;
+    return <div onClick={props.onClose} className={styles.backdrop} />;
 };
 
 const ModalOverlay = (props) => {
-  return (
-    <div className={styles.modal}>
-      <div className={styles.content}>{props.children}</div>
-    </div>
-  );
+    return (
+        <div className={styles.modal}>
+            <div>{props.children}</div>
+        </div>
+    );
 };
 
 const portalElement = document.getElementById("overlays");
 
 const Modal = (props) => {
-  return (
-    <Fragment>
-      {ReactDOM.createPortal(
-        <Backdrop onClose={props.onClose} />,
-        portalElement
-      )}
-      {ReactDOM.createPortal(
-        <ModalOverlay>{props.children}</ModalOverlay>,
-        portalElement
-      )}
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onClose={props.onClose} />,
+                portalElement
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
+        </Fragment>
+    );
 };
 
 export default Modal;
